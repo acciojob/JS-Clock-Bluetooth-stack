27d8a3afdd39512d5fda901a86c4665a0ea73e1d
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-03-31 16:29:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+const hr = document.querySelector(".hour-hand");
+const min = document.querySelector(".min-hand");
+const sec = document. querySelector(".second-hand");
+
+setInterval(()=>{
+	let day = new Date();
+	let hh = day.getHours() * 30;
+	let mm = day.getMinutes() * 6;
+	let ss = day.getSeconds() * 6;
+	
+	hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+	min.style.transform = `rotateZ(${mm}deg)`;
+	sec.style.transform = `rotateZ(${ss}deg)`;
+});
+
+
